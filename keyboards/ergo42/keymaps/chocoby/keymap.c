2b4e319b435845138c3dfce3649a61a505d078e1
Kenta Okamoto <chocoby@gmail.com>
2020-08-11 17:55:42 +0900

    Edit keymap

--- keyboards/ergo42/keymaps/chocoby/keymap.c
+++ keyboards/ergo42/keymaps/chocoby/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* BASE
  * ,------------------------------------------------.   ,------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   |  =   |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [BASE] = LAYOUT( \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,   KC_LBRC,   KC_RBRC, KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_MINS, \
+  KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,   XXXXXXX,   KC_EQL,  KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_MINS, \
   KC_LCTRL, KC_A,    KC_S,    KC_D,     KC_F,    KC_G,   S(KC_9),   S(KC_0), KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,   XXXXXXX,   XXXXXXX, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
   MO(SYMB), KC_LALT, KC_LEFT, KC_RIGHT, KC_LGUI, KC_SPC, KC_ESC,    KC_BSPC, KC_ENT, KC_RGUI, XXXXXXX, KC_LBRC, KC_RBRC,  MO(META) \
